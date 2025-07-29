236547e6e5826eb3d88db97ae97ef6372d6a42be
Maksym Parfonov <parfemax@gmail.com>
2025-07-29 10:24:27 +0300

    Implement records, custom annotations and reflection - correction

--- src/main/java/org/example/courtsystem/util/AnnotationProcessor.java
+++ src/main/java/org/example/courtsystem/util/AnnotationProcessor.java
@@ -2,13 +2,16 @@ package org.example.courtsystem.util;
 
 import org.example.courtsystem.annotations.AuthorAnnotation;
 import org.example.courtsystem.annotations.CaseMetadata;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 public class AnnotationProcessor {
+    private static final Logger logger = LogManager.getLogger(AnnotationProcessor.class);
+
     public static void processAnnotations(Class<?> clazz) {
-        System.out.println("\nProcessing annotations for: " + clazz.getSimpleName());
+        logger.info("\nProcessing annotations for: {}", clazz.getSimpleName());
 
         // Processing class annotations
         processClassAnnotations(clazz);
@@ -21,19 +24,19 @@ public class AnnotationProcessor {
         // CaseMetadata annotation
         CaseMetadata caseMetadata = clazz.getAnnotation(CaseMetadata.class);
         if (caseMetadata != null) {
-            System.out.println("Case Metadata:");
-            System.out.println("  Author: " + caseMetadata.author());
-            System.out.println("  Created: " + caseMetadata.creationDate());
-            System.out.println("  Version: " + caseMetadata.version());
+            logger.info("Case Metadata:");
+            logger.info("  Author: {}", caseMetadata.author());
+            logger.info("  Created: {}", caseMetadata.creationDate());
+            logger.info("  Version: {}", caseMetadata.version());
         }
 
         // AuthorAnnotation annotation
         AuthorAnnotation authorAnnotation = clazz.getAnnotation(AuthorAnnotation.class);
         if (authorAnnotation != null) {
-            System.out.println("Author Info:");
-            System.out.println("  Author: " + authorAnnotation.author());
-            System.out.println("  Date: " + authorAnnotation.date());
-            System.out.println("  Description: " + authorAnnotation.description());
+            logger.info("Author Info:");
+            logger.info("  Author: {}", authorAnnotation.author());
+            logger.info("  Date: {}", authorAnnotation.date());
+            logger.info("  Description: {}", authorAnnotation.description());
         }
     }
 
@@ -41,9 +44,9 @@ public class AnnotationProcessor {
         for (Method method : clazz.getDeclaredMethods()) {
             AuthorAnnotation methodAnnotation = method.getAnnotation(AuthorAnnotation.class);
             if (methodAnnotation != null) {
-                System.out.println("\nMethod '" + method.getName() + "' annotations:");
-                System.out.println("  Author: " + methodAnnotation.author());
-                System.out.println("  Date: " + methodAnnotation.date());
+                logger.info("\nMethod '{}' annotations:", method.getName());
+                logger.info("  Author: {}", methodAnnotation.author());
+                logger.info("  Date: {}", methodAnnotation.date());
             }
         }
     }
